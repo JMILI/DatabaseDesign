0cba09fceb6c0c2d60cc77a6307b280fec7c8177
JMILI <923749851@qq.com>
2019-05-29 15:57:03 +0800

    第六次5.29

--- TheProgramSRC/JMWeb04.UI/JMWeb04.UI/Controllers/HomeController.cs
+++ TheProgramSRC/JMWeb04.UI/JMWeb04.UI/Controllers/HomeController.cs
@@ -16,16 +16,18 @@ namespace JMWeb04.UI.Controllers
         }
         public IActionResult SignIn()
         {
-            user = service.QueryStudentService(student);
-            if (user == null || user.Password != student.Password)
-            {
-                return Redirect(Url.Action("PasswordError", "Errors"));
-            }
-            else
-            {
-                this.Response.Cookies.Append("StudentId", student.StudentId.ToString());
-                return View("LoginIn", user);
-            }
+            //此处分类，让储户和管理员分别登录到自己的界面
+
+            //user = service.QueryStudentService(student);
+            //if (user == null || user.Password != student.Password)
+            //{
+            //    return Redirect(Url.Action("PasswordError", "Errors"));
+            //}
+            //else
+            //{
+            //    this.Response.Cookies.Append("StudentId", student.StudentId.ToString());
+            //    return View("LoginIn", user);
+            //}
             return View();
         }
     }
